7036b76814407a8005d8e1652cc72e381db1d776
Sergey Tarakanov <jjtsksndeveloper@gmail.com>
2025-08-30 12:16:12 +0300

    restructurized v3

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/jjtsksn/polish-console-calculator/internal/calculators"
 	"github.com/jjtsksn/polish-console-calculator/internal/helpers"
@@ -18,6 +19,25 @@ func main() {
 		fmt.Print("Введите комманду: ")
 		scanner.Scan()
 		command := scanner.Text()
-		calculators.ExecuteCommand(command)
+		executeCommand(command)
+	}
+}
+
+// Функция принимает комманду в качестве аргумента и выполняет
+// код в зависимости от того, какую команду ввел пользователь
+func executeCommand(command string) {
+	switch command {
+	case "exit":
+		clearers.ClearTerminal()
+		os.Exit(0)
+	default:
+		clearers.ClearTerminal()
+		if ans, err := calculators.Calculate(command); err != nil {
+			fmt.Println(err)
+			os.Exit(0)
+		} else {
+			fmt.Println("Резльтат:", ans)
+			fmt.Println("")
+		}
 	}
 }
